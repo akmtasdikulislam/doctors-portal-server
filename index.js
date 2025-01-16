d3e958b93e1e0ca930db6abc3ac3c88c9051075c
Akm Tasdikul Islam <akmtasdikulislam@gmail.com>
2025-01-16 18:14:55 +0600

    added new packages (dotenv, firebase-admin-sdk)

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+process.removeAllListeners("warning");
+console.clear();
+
 // Import required dependencies
-const express = require("express");
-const cors = require("cors");
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import admin from "firebase-admin";
+import { initializeApp } from "firebase/app";
+import { getCurrentTime } from "./functions/getCurrentTime";
+
+// Require and configure Dotenv
+dotenv.config();
 
 // Initialize express app
 const app = express();
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Initialize Firebase Admin SDK
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 // Basic middleware setup
 app.use(express.json()); // Parse JSON bodies
@@ -20,5 +46,5 @@ app.get("/", (req, res) => {
 
 // Start the server and listen on specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`${getCurrentTime()}\tServer is running on port ${PORT}`);
 });
